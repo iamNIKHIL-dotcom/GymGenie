c9fcc72da4aa5f5933247530f885ca9f2b5cc27a
iamNIKHIL-dotcom <ummidinikhil@gmail.com>
2025-04-26 19:57:17 +0530

    convex provider added

---
+++ convex/auth/auth.config.ts
@@ -0,0 +1,10 @@
+// https://engaging-ox-51.clerk.accounts.dev
+
+export default {
+    providers: [
+      {
+        domain: "https://engaging-ox-51.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
